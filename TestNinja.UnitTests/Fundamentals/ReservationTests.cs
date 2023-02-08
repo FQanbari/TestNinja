4dd1c09bf211748b3a0b353a51d1a73fe8848478
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-02-08 21:50:38 +0330

    Test All Excecution Path

---
+++ TestNinja.UnitTests/Fundamentals/ReservationTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = true};
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void CanBeCancelledBy_CancellByUserMadeBy_ReturnTrue()
+        {
+            var reservation = new Reservation();
+            var user = new User();
+            reservation.MadeBy= user;
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void CanBeCancelledBy_OtherUser_ReturnFalse()
+        {
+            var reservation = new Reservation();
+            var user = new User();
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void CanBeCancelledBy_WhoMadeAndAdmin_ReturnTrue()
+        {
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = true};
+            reservation.MadeBy = user;
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.That(result, Is.EqualTo(true));
+        }
+    }
+}
