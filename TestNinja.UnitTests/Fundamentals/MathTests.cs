859c99ed8a77e53b866e8979bdea5d1dd2c0c632
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-02-08 22:08:55 +0330

    Use Set Up attribute

--- TestNinja.UnitTests/Fundamentals/MathTests.cs
+++ TestNinja.UnitTests/Fundamentals/MathTests.cs
@@ -11,12 +11,17 @@ namespace TestNinja.UnitTests.Fundamentals
     [TestFixture]
     public class MathTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            var _math = new TestNinja.Fundamentals.Math();
+        }
+
         [Test]
         [TestCase(1,2,3)]
         [TestCase(1,1,2)]
         public void Add_WhenCalled_ReturnSumArgument(int a, int b, int exceptionResult)
         {
-            var math = new TestNinja.Fundamentals.Math();
 
             var result = math.Add(a, b);
 
