e1832287f3a273fb59d06c5379535b44165d807e
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-02-08 21:56:03 +0330

    Writing a Simple Unit Test

---
+++ TestNinja.UnitTests/Fundamentals/MathTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        [TestCase(1,2,3)]
+        [TestCase(1,1,2)]
+        public void Add_WhenCalled_ReturnSumArgument(int a, int b, int exceptionResult)
+        {
+            var math = new TestNinja.Fundamentals.Math();
+
+            var result = math.Add(a, b);
+
+            Assert.That(result, Is.EqualTo(exceptionResult));
+        }
+    }
+}
