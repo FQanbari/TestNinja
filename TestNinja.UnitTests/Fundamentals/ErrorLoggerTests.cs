cf21b6f746305228d7c7b01bc4c66721685d8d4c
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-03-10 17:47:16 +0330

    Testing Void Methods

---
+++ TestNinja.UnitTests/Fundamentals/ErrorLoggerTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_InputIsNotNull_SetTheLastErrorProperty()
+        {
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError,Is.EqualTo("a"));
+        }
+
+    }
+}
