1a16e39683f78b008c636e12105ffc00b56c6e30
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-03-10 17:16:19 +0330

    Testing the Return Type of Methods

---
+++ TestNinja.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsGreaterThanZero_ReturnCustomer ()
+        {
+            var result = _controller.GetCustomer(1);
+
+            // Ok
+            Assert.That(result, Is.TypeOf<Ok>());
+            // Ok or one of its derivatives
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnCustomer()
+        {
+            var result = _controller.GetCustomer(0);
+
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+            // NotFound or one of its derivatives
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+        [Test]
+        public void GetCustomer_IdIsLessThanZero_ReturnCustomer()
+        {
+            var result = _controller.GetCustomer(-1);
+
+            // Ok
+            Assert.That(result, Is.TypeOf<Ok>());
+            // Ok or one of its derivatives
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
