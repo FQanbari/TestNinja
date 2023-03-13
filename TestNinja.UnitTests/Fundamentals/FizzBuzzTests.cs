e753f46f9a136b3c7064804b1877f1131c0d7146
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-03-13 20:09:03 +0330

    Exercise FizzBuzz

---
+++ TestNinja.UnitTests/Fundamentals/FizzBuzzTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+        [Test] 
+        public void GetOutput_InputIsDivisibleBy3_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5And3_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy5And3_ReturnNumber()
+        {
+            var result = FizzBuzz.GetOutput(7);
+
+            Assert.That(result, Is.EqualTo("7"));
+        }
+    }
+}
