543394bf1f03fe05c99ebc42d4f3855e4fd2e257
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-03-15 21:57:08 +0330

    Fake as Little As Possible with An Example of a Mock Abuse

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        private Product _product;
+        private Mock<ICustomer> _cutstomer;
+        [SetUp] 
+        public void SetUp() 
+        {
+            _cutstomer = new Mock<ICustomer>();
+            _product = new Product();
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Applying30PercentDiscount()
+        {
+            _product.ListPrice = 100;
+            var result = _product.GetPrice(new Customer { IsGold= true });
+
+            Assert.That(result, Is.EqualTo(70f));
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Applying30PercentDiscount2()
+        {
+            _cutstomer.Setup(c => c.IsGold).Returns(true);
+
+            _product.ListPrice = 100;
+            var result = _product.GetPrice(_cutstomer.Object);
+
+            Assert.That(result, Is.EqualTo(70f));
+        }
+    }
+}
